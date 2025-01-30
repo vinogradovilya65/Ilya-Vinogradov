1e94901e3689a03fbf438f20bb523ce411e6e2a1
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 19:05:57 +0200

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    # Do not change the line below
+    a, b = int(input("Enter a: ")), int(input("Enter b: "))
+
+    # Modify variables a and b to make the script work without errors
+
+    # Do not change the line below
+    assert a * b
