a6f110af63846debd3e27b75d3b8fae55f0944c2
vinogradovilya65 <illavinogradov500@gmail.com>
2025-01-30 18:56:05 +0200

    task2.py

---
+++ task2.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    # Do not change the 3 lines below
+    a = "name"
+    b = "is"
+    my = "my"
+
+    # Modify variables `name` and `msg` to make this script work without errors
+    # Use variables a, b, my to build a value for variable msg
+    name = "Ilya"  # Можно любое имя
+    msg = f"{my.capitalize()} {a} {b} {name}"  # "My name is Ilya"
+
+    # Do not change the line below
+    assert msg == f"My name is {name}"
